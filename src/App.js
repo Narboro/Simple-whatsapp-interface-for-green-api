b3fbd39c9785c31bd6e1e4283d928abb69cef572
Andrei.Surkov@mkcorp.com <Andrei.Surkov@mkcorp.com>
2023-05-22 17:17:10 +0300

    code push

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className='main-window'>
+        <div className='contact-list'>
+          <div className='generic-header'></div>
+          1
+        </div>
+        <div className='messages-list'>
+          <div className='generic-header'></div>
+          2
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
